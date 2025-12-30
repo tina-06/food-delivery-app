d99e5ff0e3b0fc37705f2f2c8a2a6f2486d38453
tina-06 <tina.dhage1@gmail.com>
2025-12-31 00:17:30 +0530

    Client Code

---
+++ src/assets/assets.js
@@ -0,0 +1,447 @@
+import logo from "./logo.png";
+import add_icon from "./add_icon.png";
+import order_icon from "./order_icon.png";
+import profile_image from "./profile_image.png";
+import upload_area from "./upload_area.png";
+import parcel_icon from "./parcel_icon.png";
+import basket_icon from "./basket_icon.png";
+import search_icon from "./search_icon.png";
+import menu_1 from "./menu_1.png";
+import menu_2 from "./menu_2.png";
+import menu_3 from "./menu_3.png";
+import menu_4 from "./menu_4.png";
+import menu_5 from "./menu_5.png";
+import menu_6 from "./menu_6.png";
+import menu_7 from "./menu_7.png";
+import menu_8 from "./menu_8.png";
+import food_1 from "./food_1.png";
+import food_2 from "./food_2.png";
+import food_3 from "./food_3.png";
+import food_4 from "./food_4.png";
+import food_5 from "./food_5.png";
+import food_6 from "./food_6.png";
+import food_7 from "./food_7.png";
+import food_8 from "./food_8.png";
+import food_9 from "./food_9.png";
+import food_10 from "./food_10.png";
+import food_11 from "./food_11.png";
+import food_12 from "./food_12.png";
+import food_13 from "./food_13.png";
+import food_14 from "./food_14.png";
+import food_15 from "./food_15.png";
+import food_16 from "./food_16.png";
+import food_17 from "./food_17.png";
+import food_18 from "./food_18.png";
+import food_19 from "./food_19.png";
+import food_20 from "./food_20.png";
+import food_21 from "./food_21.png";
+import food_22 from "./food_22.png";
+import food_23 from "./food_23.png";
+import food_24 from "./food_24.png";
+import food_25 from "./food_25.png";
+import food_26 from "./food_26.png";
+import food_27 from "./food_27.png";
+import food_28 from "./food_28.png";
+import food_29 from "./food_29.png";
+import food_30 from "./food_30.png";
+import food_31 from "./food_31.png";
+import food_32 from "./food_32.png";
+import rating_starts from "./rating_starts.png";
+import add_icon_white from "./add_icon_white.png";
+import remove_icon_red from "./remove_icon_red.png";
+import add_icon_green from "./add_icon_green.png";
+import facebook_icon from "./facebook_icon.png";
+import twitter_icon from "./twitter_icon.png";
+import linkedin_icon from "./linkedin_icon.png";
+import play_store from "./play_store.png";
+import app_store from "./app_store.png";
+import cross_icon from "./cross_icon.png";
+
+export const assets = {
+  logo,
+  add_icon,
+  order_icon,
+  profile_image,
+  upload_area,
+  parcel_icon,
+  basket_icon,
+  search_icon,
+  menu_1,
+  menu_2,
+  menu_3,
+  menu_4,
+  menu_5,
+  menu_6,
+  menu_7,
+  menu_8,
+  food_1,
+  food_2,
+  food_3,
+  food_4,
+  food_5,
+  food_6,
+  food_7,
+  food_8,
+  food_9,
+  food_10,
+  food_11,
+  food_12,
+  food_13,
+  food_14,
+  food_15,
+  food_16,
+  food_17,
+  food_18,
+  food_19,
+  food_20,
+  food_21,
+  food_22,
+  food_23,
+  food_24,
+  food_25,
+  food_26,
+  food_27,
+  food_28,
+  food_29,
+  food_30,
+  food_31,
+  food_32,
+  rating_starts,
+  add_icon_white,
+  remove_icon_red,
+  add_icon_green,
+  facebook_icon,
+  twitter_icon,
+  linkedin_icon,
+  play_store,
+  app_store,
+  cross_icon
+};
+
+export const menu_list = [
+  {
+    menu_name: "Salad",
+    menu_image: menu_1,
+  },
+  {
+    menu_name: "Rolls",
+    menu_image: menu_2,
+  },
+  {
+    menu_name: "Desserts",
+    menu_image: menu_3,
+  },
+  {
+    menu_name: "Sandwich",
+    menu_image: menu_4,
+  },
+  {
+    menu_name: "Cakes",
+    menu_image: menu_5,
+  },
+  {
+    menu_name: "Authentic",
+    menu_image: menu_6,
+  },
+  {
+    menu_name: "Pasta",
+    menu_image: menu_7,
+  },
+  {
+    menu_name: "Bowls",
+    menu_image: menu_8,
+  },
+];
+
+export const food_list = [
+  {
+    id: "1",
+    name: "Greek Salad",
+    image: food_1,
+    price: 20,
+    description:
+      "Food provides essential nutrients for overall health and well-being",
+    category: "Salad",
+  },
+  {
+    id: "2",
+    name: "Veg Salad",
+    image: food_2,
+    price: 18,
+    description:
+      "Food provides essential nutrients for overall health and well-being",
+    category: "Salad",
+  },
+  {
+    id: "3",
+    name: "Plain salad",
+    image: food_3,
+    price: 10,
+    description:
+      "Food provides essential nutrients for overall health and well-being",
+    category: "Salad",
+  },
+  {
+    id: "4",
+    name: "Spicy Salad",
+    image: food_4,
+    price: 25,
+    description:
+      "Food provides essential nutrients for overall health and well-being",
+    category: "Salad",
+  },
+  {
+    id: "5",
+    name: "Cheese Rolls",
+    image: food_5,
+    price: 22,
+    description:
+      "Food provides essential nutrients for overall health and well-being",
+    category: "Rolls",
+  },
+  {
+    id: "6",
+    name: "Makhani Rolls",
+    image: food_6,
+    price: 15,
+    description:
+      "Food provides essential nutrients for overall health and well-being",
+    category: "Rolls",
+  },
+  {
+    id: "7",
+    name: "Chicken Rolls",
+    image: food_7,
+    price: 20,
+    description:
+      "Food provides essential nutrients for overall health and well-being",
+    category: "Rolls",
+  },
+  {
+    id: "8",
+    name: "Creamy Rolls",
+    image: food_8,
+    price: 29,
+    description:
+      "Food provides essential nutrients for overall health and well-being",
+    category: "Rolls",
+  },
+  {
+    id: "9",
+    name: "Yoghurt",
+    image: food_9,
+    price: 30,
+    description:
+      "Food provides essential nutrients for overall health and well-being",
+    category: "Desserts",
+  },
+  {
+    id: "10",
+    name: "Greek Yoghurt",
+    image: food_10,
+    price: 25,
+    description:
+      "Food provides essential nutrients for overall health and well-being",
+    category: "Desserts",
+  },
+  {
+    id: "11",
+    name: "Strawberry Custard",
+    image: food_11,
+    price: 28,
+    description:
+      "Food provides essential nutrients for overall health and well-being",
+    category: "Desserts",
+  },
+  {
+    id: "12",
+    name: "Cotton Candy Blush",
+    image: food_12,
+    price: 16,
+    description:
+      "Food provides essential nutrients for overall health and well-being",
+    category: "Desserts",
+  },
+  {
+    id: "13",
+    name: "Corn Sandwich",
+    image: food_13,
+    price: 22,
+    description:
+      "Food provides essential nutrients for overall health and well-being",
+    category: "Sandwich",
+  },
+  {
+    id: "14",
+    name: "Veg Sandwich",
+    image: food_14,
+    price: 20,
+    description:
+      "Food provides essential nutrients for overall health and well-being",
+    category: "Sandwich",
+  },
+  {
+    id: "15",
+    name: "Greek Sandwich",
+    image: food_15,
+    price: 30,
+    description:
+      "Food provides essential nutrients for overall health and well-being",
+    category: "Sandwich",
+  },
+  {
+    id: "16",
+    name: "Burrito",
+    image: food_16,
+    price: 32,
+    description:
+      "Food provides essential nutrients for overall health and well-being",
+    category: "Authentic",
+  },
+  {
+    id: "17",
+    name: "Cupcake",
+    image: food_17,
+    price: 20,
+    description:
+      "Food provides essential nutrients for overall health and well-being",
+    category: "Cakes",
+  },
+  {
+    id: "18",
+    name: "Cherry cake",
+    image: food_18,
+    price: 27,
+    description:
+      "Food provides essential nutrients for overall health and well-being",
+    category: "Cakes",
+  },
+  {
+    id: "19",
+    name: "Blueberry Blush",
+    image: food_19,
+    price: 30,
+    description:
+      "Food provides essential nutrients for overall health and well-being",
+    category: "Cakes",
+  },
+  {
+    id: "20",
+    name: "Cheese cake",
+    image: food_20,
+    price: 30,
+    description:
+      "Food provides essential nutrients for overall health and well-being",
+    category: "Cakes",
+  },
+  {
+    id: "21",
+    name: "Italian burrito",
+    image: food_21,
+    price: 26,
+    description:
+      "Food provides essential nutrients for overall health and well-being",
+    category: "Authentic",
+  },
+  {
+    id: "22",
+    name: "Egg smash",
+    image: food_22,
+    price: 32,
+    description:
+      "Food provides essential nutrients for overall health and well-being",
+    category: "Authentic",
+  },
+  {
+    id: "23",
+    name: "Veggie Masata",
+    image: food_23,
+    price: 22,
+    description:
+      "Food provides essential nutrients for overall health and well-being",
+    category: "Authentic",
+  },
+  {
+    id: "24",
+    name: "Greek Veggie Bowl",
+    image: food_24,
+    price: 30,
+    description:
+      "Food provides essential nutrients for overall health and well-being",
+    category: "Bowls",
+  },
+  {
+    id: "25",
+    name: "Creamy Pasta",
+    image: food_25,
+    price: 32,
+    description:
+      "Food provides essential nutrients for overall health and well-being",
+    category: "Pasta",
+  },
+  {
+    id: "26",
+    name: "Masala Pasta",
+    image: food_26,
+    price: 28,
+    description:
+      "Food provides essential nutrients for overall health and well-being",
+    category: "Pasta",
+  },
+  {
+    id: "27",
+    name: "White Sause Pasta",
+    image: food_27,
+    price: 40,
+    description:
+      "Food provides essential nutrients for overall health and well-being",
+    category: "Pasta",
+  },
+  {
+    id: "28",
+    name: "Paper Glass Noodles",
+    image: food_28,
+    price: 30,
+    description:
+      "Food provides essential nutrients for overall health and well-being",
+    category: "Pasta",
+  },
+  {
+    id: "29",
+    name: "Chinese platter",
+    image: food_29,
+    price: 20,
+    description:
+      "Food provides essential nutrients for overall health and well-being",
+    category: "Bowls",
+  },
+  {
+    id: "30",
+    name: "Spicy Noodle Bowl",
+    image: food_30,
+    price: 24,
+    description:
+      "Food provides essential nutrients for overall health and well-being",
+    category: "Bowls",
+  },
+  {
+    id: "31",
+    name: "Arabica Pasta",
+    image: food_31,
+    price: 30,
+    description:
+      "Food provides essential nutrients for overall health and well-being",
+    category: "Bowls",
+  },
+  {
+    id: "32",
+    name: "Greek salad ramen",
+    image: food_32,
+    price: 32,
+    description:
+      "Food provides essential nutrients for overall health and well-being",
+    category: "Bowls",
+  },
+];
+
+export const url = "http://localhost:4000";
