591ab541b9b1c72585396d8b16c40308b47b802d
tina-06 <tina.dhage1@gmail.com>
2025-12-31 00:48:19 +0530

    Fix routing and base path

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from 'react-router-dom'
 import StoreContextProvider from './context/StoreContext.jsx'
 
 createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
+  <BrowserRouter basename="/food-delivery-app">
     <StoreContextProvider>
       <App />
     </StoreContextProvider>
