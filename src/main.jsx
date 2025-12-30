238099ad1a1edcdaa89941f5c300105f01595cb7
tina-06 <tina.dhage1@gmail.com>
2025-12-31 01:01:34 +0530

    Cleanup for Vercel

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,14 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import "./index.css";
-import App from "./App.jsx";
-import { HashRouter } from "react-router-dom";
-import StoreContextProvider from "./context/StoreContext.jsx";
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import { BrowserRouter } from 'react-router-dom'
+import StoreContextProvider from './context/StoreContext.jsx'
 
 createRoot(document.getElementById("root")).render(
-  <HashRouter>
+  <BrowserRouter>
     <StoreContextProvider>
       <App />
     </StoreContextProvider>
-  </HashRouter>
+  </BrowserRouter>
 );
